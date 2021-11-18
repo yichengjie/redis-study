d12098e8807c69234e4c1b4b6980570ec8f62c39
yichengjie <626659321@qq.com>
2021-11-18 17:45:58 +0800

    save

--- redis-stream/src/main/java/com/yicj/redis/model/PromoRequest.java
+++ redis-stream/src/main/java/com/yicj/redis/model/PromoRequest.java
@@ -1,9 +1,12 @@
 package com.yicj.redis.model;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.Serializable;
 import java.util.List;
 
+@Slf4j
 @Data
 public class PromoRequest implements Serializable {
 
--- redis-stream/src/test/java/com/yicj/redis/utils/CommonUtilTest.java
+++ redis-stream/src/test/java/com/yicj/redis/utils/CommonUtilTest.java
@@ -1,6 +1,12 @@
 package com.yicj.redis.utils;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 
 public class CommonUtilTest {
@@ -11,5 +17,29 @@ public class CommonUtilTest {
         byte[] myGroups = serializer.serialize("myGroup");
         String str = new String(myGroups);
         System.out.println(str);
+    }
+
+
+    @Test
+    public void test1(){
+        User u1 = new User("u1", Arrays.asList(new Address("addr1"), new Address("addr2"), new Address("addr3"))) ;
+        User u2 = new User("u2", Arrays.asList(new Address("addr21"), new Address("addr22"), new Address("addr23"))) ;
+        User u3 = new User("u3", Arrays.asList(new Address("addr31"), new Address("addr32"), new Address("addr33"))) ;
+        List<User> users = Arrays.asList(u1, u2, u3) ;
+        List<String> addrNames = users.stream().flatMap(u -> u.getAddrs().stream().map(Address::getName)).distinct().collect(Collectors.toList());
+        System.out.println(addrNames);
+    }
+
+    @Data
+    @AllArgsConstructor
+    class User{
+        private String username ;
+        private List<Address> addrs ;
+    }
+
+    @Data
+    @AllArgsConstructor
+    class Address{
+        private String name ;
     }
 }
